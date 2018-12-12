c8053381f7271e819a345a46c3013afea41ff110
💻 💪 😉 James Hood <james@jlhood.com>
2018-12-12 00:33:31 -0800

    checkout-pipeline bugfixes

--- examples/ecommerce-app/checkout-pipeline/src/process_records.py
+++ examples/ecommerce-app/checkout-pipeline/src/process_records.py
@@ -4,6 +4,7 @@
 import lambdainit  # noqa: F401
 
 import boto3
+import json
 
 import config
 import lambdalogging
@@ -15,15 +16,15 @@ TABLE = boto3.resource('dynamodb').Table(config.TABLE_NAME)
 
 def handler(event, context):
     """Convert checkout events to Orders table records."""
-    LOG.debug('Received event: {}', event)
+    LOG.debug('Received event: %s', event)
 
     if 'Records' not in event:
         LOG.info('No records in event')
         return
 
-    for checkout_event in event['Records']:
+    for record in event['Records']:
         TABLE.put_item(
-            Item=_to_item(checkout_event)
+            Item=_to_item(json.loads(record['body']))
         )
 
 
--- examples/ecommerce-app/checkout-pipeline/test/unit/test_process_records.py
+++ examples/ecommerce-app/checkout-pipeline/test/unit/test_process_records.py
@@ -1,32 +1,37 @@
 import pytest
+import json
+
 import process_records
 
 
 def test_handler(mocker):
     mocker.patch.object(process_records, 'TABLE')
-    event = {
-        'Records': [
-            {
-                'id': 1,
-                'date': '2018-12-15T23:41:11-08:00',
-                'status': 'confirmed',
-                'customer': {
-                    'id': 100
-                }
-            },
-            {
-                'id': 2,
-                'date': '2018-12-16T23:41:11-08:00',
-                'status': 'confirmed',
-                'customer': {
-                    'id': 101
-                }
+    checkout_events = [
+        {
+            'id': 1,
+            'date': '2018-12-15T23:41:11-08:00',
+            'status': 'confirmed',
+            'customer': {
+                'id': 100
             }
-        ]
-    }
-    process_records.handler(event, None)
+        },
+        {
+            'id': 2,
+            'date': '2018-12-16T23:41:11-08:00',
+            'status': 'confirmed',
+            'customer': {
+                'id': 101
+            }
+        }
+    ]
+    process_records.handler(_input_event(checkout_events), None)
     process_records.TABLE.put_item.assert_called()
     assert process_records.TABLE.put_item.call_count == 2
+
+
+def _input_event(checkout_events):
+    records = [{'body': json.dumps(e)} for e in checkout_events]
+    return {'Records': records}
 
 
 def test_handler_no_records(mocker):
